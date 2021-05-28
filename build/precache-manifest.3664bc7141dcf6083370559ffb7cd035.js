bcdaf6a88a61454cb24a295de903a7fa807069f8
oforduhh <oforduhh@gmail.com>
2021-05-28 21:30:29 +0100

    jgjgjgg

---
+++ build/precache-manifest.3664bc7141dcf6083370559ffb7cd035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26bc8e73c3d2eecb5072e603b1a868d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bae1b73f6e2d90cc5039",
+    "url": "/static/css/main.9a4e297a.chunk.css"
+  },
+  {
+    "revision": "6a6c3aad92c2a718314b",
+    "url": "/static/js/2.59a200a0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.59a200a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae1b73f6e2d90cc5039",
+    "url": "/static/js/main.c9864522.chunk.js"
+  },
+  {
+    "revision": "c3655119f17e286e58a8",
+    "url": "/static/js/runtime-main.dbfcc0d1.js"
+  }
+]);
